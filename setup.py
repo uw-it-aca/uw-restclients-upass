9078429f49ba734b967d5819ca11ea66ebefcced
Fang Lin <fanglin@uw.edu>
2017-10-27 10:16:32 -0700

    upd pypi server

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author="UW-IT AXDD",
     author_email="aca-it@uw.edu",
     include_package_data=True,
-    install_requires=['UW-RestClients-Core>=0.8.9,<1.0',
+    install_requires=['UW-RestClients-Core<1.0',
                       ],
     license='Apache License, Version 2.0',
     description=('A library for connecting to the UPass API'),
@@ -34,5 +34,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
     ],
 )
