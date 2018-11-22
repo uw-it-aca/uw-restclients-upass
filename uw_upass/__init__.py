133bc7bfab3c6be7cd0dad6a8e66025b38b02f09
Fang Lin <fanglin@uw.edu>
2018-11-22 15:23:45 -0800

    use format() instead of %s

--- uw_upass/__init__.py
+++ uw_upass/__init__.py
@@ -3,6 +3,7 @@ This is the interface for interacting with the UPass service.
 
 """
 
+from urllib.parse import urlencode
 from uw_upass.dao import UPass_DAO
 from restclients_core.exceptions import DataFailureException
 from uw_upass.models import UPassStatus, CURRENT, NOT_CURRENT
@@ -21,12 +22,12 @@ def get_upass_status(netid):
 
     if len(response_data) == 0 or\
             not(CURRENT in response_data or NOT_CURRENT in response_data):
-        raise Exception("%s Unexpected Response Data: %s" %
-                        (url, response_data))
+        raise Exception("{} Unexpected Response Data: {}".format(
+            url, response_data))
 
     status = UPassStatus.create(response_data)
     return status
 
 
 def get_upass_url(netid):
-    return ("/MyUWUpass/MyUWUpass.aspx?id=%s" % netid)
+    return "/MyUWUpass/MyUWUpass.aspx?{}".format(urlencode({"id": netid}))
