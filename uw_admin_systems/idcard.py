d8d1bdb2b211936dd5ccf7fb894f59a8391cf9c7
Fang Lin <fanglin@uw.edu>
2025-02-21 11:33:16 -0800

    Support upass and idcard

--- uw_admin_systems/idcard.py
+++ uw_admin_systems/idcard.py
@@ -8,13 +8,13 @@ This is the interface for interacting with the UPass service.
 
 import json
 from uw_admin_systems import get_resource
-from uw_admin_systems.models import UPassStatus
+from uw_admin_systems.models import IDcardStatus
 
 
-def get_upass_status(netid):
+def get_idcard_status(netid):
     data = get_resource(get_upass_url(netid))
-    return UPassStatus.create(json.loads(data))
+    return IDcardStatus.create(json.loads(data))
 
 
 def get_upass_url(netid):
-    return f"/upassdataws/api/person/v1/membershipstatus/{netid}"
+    return f"/idcarddataws/api/person/v1/eligibility/{netid}"
--- uw_admin_systems/models.py
+++ uw_admin_systems/models.py
@@ -5,16 +5,43 @@ import json
 from restclients_core import models
 
 
-class UPassStatus(models.Model):
-    active_employee_membership = models.BooleanField(
-        null=True, default=None)
-    active_student_membership = models.BooleanField(
-        null=True, default=None)
+class IDcardStatus(models.Model):
+    not_eligible = models.BooleanField(null=True, default=None)
+    employee_eligible = models.BooleanField(null=True, default=None)
+    retiree_eligible = models.BooleanField(null=True, default=None)
+    student_eligible = models.BooleanField(null=True, default=None)
 
     def json_data(self):
         return {
-            'active_employee_membership': self.active_employee_membership,
-            'active_student_membership': self.active_student_membership,
+            "not_eligible": self.not_eligible,
+            "employee_eligible": self.employee_eligible,
+            "retiree_eligible": self.retiree_eligible,
+            "student_eligible": self.student_eligible,
+        }
+
+    @classmethod
+    def create(cls, data):
+        status = cls()
+        emp_value = data.get("empCardElig", "")
+        status.employee_eligible = emp_value == "Y"
+        status.retiree_eligible = emp_value == "R"
+        stud_value = data.get("stdtCardElig", "")
+        status.student_eligible = stud_value == "Y"
+        status.not_eligible = emp_value == "N" and stud_value == "N"
+        return status
+
+    def __str__(self):
+        return json.dumps(self.json_data())
+
+
+class UPassStatus(models.Model):
+    active_employee_membership = models.BooleanField(null=True, default=None)
+    active_student_membership = models.BooleanField(null=True, default=None)
+
+    def json_data(self):
+        return {
+            "active_employee_membership": self.active_employee_membership,
+            "active_student_membership": self.active_student_membership,
         }
 
     @classmethod
@@ -22,10 +49,12 @@ class UPassStatus(models.Model):
         status = cls()
         if "activeEmployeeMembership" in data:
             status.active_employee_membership = data.get(
-                'activeEmployeeMembership', False)
+                "activeEmployeeMembership", False
+            )
         if "activeStudentMembership" in data:
             status.active_student_membership = data.get(
-                'activeStudentMembership', False)
+                "activeStudentMembership", False
+            )
         return status
 
     def __str__(self):
